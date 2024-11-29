515bacdc62eec43a01e36637dd16361337315fca
chris <Mileschris466@gmail.com>
2024-11-29 13:14:57 +0300

    for loop

--- list.py
+++ list.py
@@ -1,2 +1,17 @@
 hotel = ["safari","Panari","Serena","Kempiski","Boston"]
 hotel(2);
+
+# Testing the function
+count = 0
+while count<5:
+print("count")
+count +=1
+
+# For Loop.
+
+for i in  range(5):
+    if i == 3:
+        break
+    elif i == 2:
+        continue
+    print(i)
