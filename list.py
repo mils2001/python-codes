6f457c833326da9c573b8ca6ddd07ffbee70f003
chris <Mileschris466@gmail.com>
2024-11-29 13:29:45 +0300

    corrections

--- list.py
+++ list.py
@@ -7,11 +7,20 @@ while count<5:
 print("count")
 count +=1
 
-# For Loop.
-
+# For Loop Control Statement.
 for i in  range(5):
     if i == 3:
         break
     elif i == 2:
         continue
     print(i)
+
+    # conditional statement
+
+    x = 10
+    if x > 5:
+        print("x is greater than 5")
+        elif==5:
+        print("x is greater than")
+        else:
+              print("x is less than 5")
