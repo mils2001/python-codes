9bbad5cc3f56a3c29e4b0288141db81d413d0403
chris <Mileschris466@gmail.com>
2024-12-16 12:08:33 +0300

    person

--- list.py
+++ list.py
@@ -2,6 +2,12 @@ hotel = ["safari","Panari","Serena","Kempiski","Boston"]
 hotel(2);
 
 
-
+person = {
+    "name": " chris",
+     "age": 35,
+     "city": " Nairobi"
+}
+print(person["name"])
+person["age"]
 
    
