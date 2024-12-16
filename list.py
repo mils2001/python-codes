6c0db7d5aa7407a6a20ccf6ac164f5c9b3c24d4d
chris <Mileschris466@gmail.com>
2024-12-16 12:03:24 +0300

    python

--- list.py
+++ list.py
@@ -1,26 +1,7 @@
 hotel = ["safari","Panari","Serena","Kempiski","Boston"]
 hotel(2);
 
-# Testing the function
-count = 0
-while count<5:
-print("count")
-count +=1
 
-# For Loop Control Statement.
-for i in  range(5):
-    if i == 3:
-        break
-    elif i == 2:
-        continue
-    print(i)
 
-    # conditional statement
 
-    x = 10
-    if x > 5:
-        print("x is greater than 5")
-        elif==5:
-        print("x is greater than")
-        else:
-              print("x is less than 5")
+   
